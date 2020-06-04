51d17da3c94fa3e8e75a264b0950ab53d9928d53
Boyan Levchev <boyanlevchev@gmail.com>
2020-06-04 15:10:11 +0100

    ready to push to heroku - test 7

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 // const path = require('path')// Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'froala-fun/build')))// Anything that doesn't match the above, send back index.html
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/froala-fun/build/index.html'))
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/froala-fun/build/index.html'))
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

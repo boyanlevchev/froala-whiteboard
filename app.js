92566f55cb7e3304751497abb05abb8d1e39b7ef
Boyan Levchev <boyanlevchev@gmail.com>
2020-06-04 13:48:18 +0100

    ready to push to heroku - test 4

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var frofroRouter = require('./routes/get_frofro');
 
 var app = express();
 
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -19,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'froala-fun/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

81e7b2553b0b4ccdadd65f7eb0c635952ec7baff
Boyan Levchev <boyanlevchev@gmail.com>
2020-06-17 15:09:40 +0100

    it worked on the previous one! but maybe that's because I accidentally had access open to * origins?

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
+// var cors = require('cors');
 // var request = require('request')
 
 var indexRouter = require('./routes/index');
--- routes/get_signature.js
+++ routes/get_signature.js
@@ -3,27 +3,32 @@ var router = express.Router();
 var cors = require('cors');
 require('dotenv').config();
 
-// var allowedOrigins = ['http://localhost:3000',
-//                       'http://froala-whiteboard.herokuapp.com/',
-//                       'https://froala-whiteboard.herokuapp.com/',
-//                       'http://floopshoop.wpcomstaging.com/contact/',
-//                       'https://floopshoop.wpcomstaging.com/contact/']
+var allowedOrigins = [
+                      'http://localhost:3000',
+                      'http://froala-whiteboard.herokuapp.com/',
+                      'https://froala-whiteboard.herokuapp.com/',
+                      'http://floopshoop.wpcomstaging.com/contact/',
+                      'https://floopshoop.wpcomstaging.com/contact/',
+                      'http://floopshoop.wpcomstaging.com/',
+                      'https://floopshoop.wpcomstaging.com'
+                      ]
 
-// var corsOptions = {
-//   origin: function(origin, callback){
-//     // allow requests with no origin
-//     // (like mobile apps or curl requests)
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){
-//       var msg = 'The CORS policy for this site does not ' +
-//                 'allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   },
-//   optionsSuccessStatus: 200
-// }
+var corsOptions = {
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  optionsSuccessStatus: 200
+}
 
+router.all('*', cors(corsOptions))
 
 var FroalaEditor = require('wysiwyg-editor-node-sdk');
 
